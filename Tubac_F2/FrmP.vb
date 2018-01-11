3613dbb6a1c11d4a117a8158aa80fc8f48a2b48c
crissdua <crissdua@hotmail.com>
2018-01-11 16:39:34 -0600

    funcional

--- Tubac_F2/FrmP.vb
+++ Tubac_F2/FrmP.vb
@@ -1,39 +1,24 @@
 ﻿Imports System.Windows.Forms
 Imports System.IO
 Imports System.Data.SqlClient
-Imports System.Data
-Imports System.Drawing.Text
-Imports CrystalDecisions.Shared
-Imports CrystalDecisions.CrystalReports.Engine
-Imports CrystalDecisions.Windows.Forms
-Imports CrystalDecisions.ReportSource
-Imports System.Web.UI.WebControls
+Imports Tubac_F2.BatchsFase2
+Imports SAPbobsCOM
+Imports SAPbouiCOM
+
 Public Class FrmP
 #Region "Variables"
     Public con As New Conexion
-    Dim objectCode As String
-    Dim itemcode As String
+    Public itemcode As String
+    Dim valBarCode As String
     Dim oCompany As SAPbobsCOM.Company
     Dim connectionString As String = Conexion.ObtenerConexion.ConnectionString
-    Public Shared PO As SAPbobsCOM.Documents
-    Public Shared GoodsReceiptPO As SAPbobsCOM.Documents
-    Public Shared SQL_Conexion As SqlConnection = New SqlConnection()
-
-#Region "Listas"
-    Dim batch As New List(Of String)
-    Dim descripcion As New List(Of String)
-    Dim anchotira As New List(Of Double)
-    Dim pesoreal As New List(Of Double)
-    Dim bobina As New List(Of String)
-    Dim heat As New List(Of String)
-    Dim coil As New List(Of String)
-    Dim ordencorte As New List(Of String)
-#End Region
-#Region "Fuentes"
-    Private _Font As Font
-    Private PATH_FONTS As String = Application.StartupPath + "\Fonts"
-#End Region
+    Public Ready As Boolean
     Private Const CP_NOCLOSE_BUTTON As Integer = &H200
+    Public Shared oInvGenExit As SAPbobsCOM.Documents
+    Public Shared SQL_Conexion As SqlConnection = New SqlConnection()
+    Public Shared ba As New List(Of String)
+    Public Shared quantity As New List(Of Integer)
+
 #End Region
     Protected Overloads Overrides ReadOnly Property CreateParams() As CreateParams
         Get
@@ -70,67 +55,34 @@ Public Class FrmP
         con.ObtenerConexion.Close()
     End Sub
 
-    Private Sub imprime(bat As String, desc As String, anch As Double, pes As Double, bob As String, het As String, coi As String, ordr As String)
-        Dim Report1 As New CrystalDecisions.CrystalReports.Engine.ReportDocument()
-        Report1.PrintOptions.PaperOrientation = PaperOrientation.Portrait
-        Report1.Load("C:\Users\Cristhiam\Desktop\Informe3.rpt", CrystalDecisions.Shared.OpenReportMethod.OpenReportByDefault.OpenReportByDefault)
-        Report1.SetParameterValue("CodBatch", bat)
-        'Report1.SetParameterValue("CodBatch", txtBarcode.Text)
-        Report1.SetParameterValue("descripcion", desc)
-        Report1.SetParameterValue("anchotira", anch)
-        Report1.SetParameterValue("pesoreal", pes)
-        Report1.SetParameterValue("bobina", bob)
-        Report1.SetParameterValue("heat", het)
-        Report1.SetParameterValue("coil", coi)
-        Report1.SetParameterValue("ordencorte", ordr)
-        Report1.SetParameterValue("fechacorte", Now.ToShortDateString)
-        'CrystalReportViewer1.ReportSource = Report1
-        Report1.PrintToPrinter(1, False, 0, 0)
-    End Sub
-    Private Sub generaEntrada()
-        batch.Clear()
-        descripcion.Clear()
-        anchotira.Clear()
-        pesoreal.Clear()
-        bobina.Clear()
-        heat.Clear()
-        coil.Clear()
-        ordencorte.Clear()
+    Private Sub generaEntrada(docE As String)
+
         Dim iResult As Integer = -1
-        Dim iResult2 As Integer = -1
-        Dim sResult As String = String.Empty
-        Dim sOutput As String = String.Empty
+        Dim contlinea As Integer
         Dim sql As String
         Dim oRecordSet As SAPbobsCOM.Recordset
-        Dim sql2 As String
-        Dim oRecordSet2 As SAPbobsCOM.Recordset
-        Dim docentry As String
+        Dim oReturn As Integer = -1
 
         Try
-            '------------------OBTIENE DOCENTRY------------
-            sql = ("SELECT T0.DocEntry FROM OPOR T0 WHERE T0.DocNum = '" + txtOrder.Text + "'")
+            '--------------------------------------------------------------------------------------------------------------
+            'EMISION DE PRODUCCION -----------------------------------------------------------------------------------------
+            '--------------------------------------------------------------------------------------------------------------
+            'Contador de lineas para la orden
+            sql = ("SELECT count(T0.DocNum) FROM OWOR T0  INNER JOIN WOR1 T1 ON T0.DocEntry = T1.DocEntry WHERE T0.DocNum = '" + docE + "'")
             oRecordSet = con.oCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset)
             oRecordSet.DoQuery(sql)
             If oRecordSet.RecordCount > 0 Then
-                docentry = oRecordSet.Fields.Item(0).Value
+                contlinea = oRecordSet.Fields.Item(0).Value
             End If
             System.Runtime.InteropServices.Marshal.ReleaseComObject(oRecordSet)
             oRecordSet = Nothing
             GC.Collect()
 
-            PO = con.oCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.oPurchaseOrders)
-            GoodsReceiptPO = con.oCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.oPurchaseDeliveryNotes)
+            Dim frm As New Tubac_F2.BatchsFase2
+            oInvGenExit = con.oCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.oInventoryGenExit)
+            Dim wo As ProductionOrders = con.oCompany.GetBusinessObject(BoObjectTypes.oProductionOrders)
+            wo.GetByKey(docE)
 
-            '----------------------------------------------
-            PO.GetByKey(docentry)
-            '----------------------------------------------
-
-            GoodsReceiptPO.CardCode = PO.CardCode
-            GoodsReceiptPO.CardName = PO.CardName
-            '----------- LINEAS -----------------------------
-
-            Dim itemcode As String
-            Dim quantity As Double
             Dim i As DataGridViewCheckBoxColumn = New DataGridViewCheckBoxColumn()
             Dim existe As Boolean = DGV2.Columns.Cast(Of DataGridViewColumn).Any(Function(x) x.Name = "CHK")
             If existe = False Then
@@ -140,79 +92,57 @@ Public Class FrmP
                 i.Width = 32
                 i.DisplayIndex = 0
             End If
-            Dim result As Integer = MessageBox.Show("Desea Ingresar la Orden?", "Atencion", MessageBoxButtons.YesNoCancel)
+            Dim result As Integer = MessageBox.Show("Desea Ingresar la Emision?", "Atencion", MessageBoxButtons.YesNoCancel)
             If result = DialogResult.Cancel Then
                 MessageBox.Show("Cancelado")
             ElseIf result = DialogResult.No Then
                 MessageBox.Show("No se realizara la orden")
             ElseIf result = DialogResult.Yes Then
+
+                oInvGenExit.DocDate = Now ' it could be date.Today.
+                oInvGenExit.DocType = SAPbobsCOM.BoDocumentTypes.dDocument_Items
+
+                Dim contbase As Integer = 0
                 For Each row As DataGridViewRow In DGV2.Rows
                     Dim chk As DataGridViewCheckBoxCell = row.Cells("CHK")
                     If chk.Value IsNot Nothing AndAlso chk.Value = True Then
-
-                        PO.Lines.SetCurrentLine(DGV2.Rows(chk.RowIndex).Cells.Item(3).Value.ToString)
-                        PO.Lines.LineStatus = SAPbobsCOM.BoStatus.bost_Close
-                        'GoodsReceiptPO.Lines.ItemCode = DGV2.Rows(chk.RowIndex).Cells.Item(1).Value.ToString
-                        'GoodsReceiptPO.Lines.ItemDescription = PO.Lines.ItemDescription
-                        GoodsReceiptPO.Lines.Quantity = DGV2.Rows(chk.RowIndex).Cells.Item(2).Value.ToString
-                        GoodsReceiptPO.Lines.BaseEntry = PO.DocEntry
-                        GoodsReceiptPO.Lines.BaseLine = DGV2.Rows(chk.RowIndex).Cells.Item(3).Value.ToString
-                        GoodsReceiptPO.Lines.BaseType = Convert.ToInt32(PO.DocObjectCodeEx)
-                        GoodsReceiptPO.Lines.BatchNumbers.SetCurrentLine(0)
-                        GoodsReceiptPO.Lines.BatchNumbers.BatchNumber = "batchIMP" & DGV2.Rows(chk.RowIndex).Cells.Item(1).Value.ToString
-                        '-----------------------------------------------------------------------------llena listas de datos
-                        batch.Add("batchIMP" & DGV2.Rows(chk.RowIndex).Cells.Item(1).Value.ToString)
-                        descripcion.Add(PO.Lines.ItemDescription)
-                        anchotira.Add(PO.Lines.UserFields.Fields.Item("U_ancho").Value)
-                        pesoreal.Add(PO.Lines.UserFields.Fields.Item("U_peso").Value)
-                        bobina.Add(PO.Lines.UserFields.Fields.Item("U_bobina").Value)
-                        heat.Add(PO.Lines.UserFields.Fields.Item("U_heat").Value)
-                        coil.Add(PO.Lines.UserFields.Fields.Item("U_coil").Value)
-                        ordencorte.Add(PO.DocNum)
-                        '---------------------------------------------------------------------------------------------------
-                        GoodsReceiptPO.Lines.BatchNumbers.Quantity = Convert.ToDouble(DGV2.Rows(chk.RowIndex).Cells.Item(2).Value.ToString)
-                        GoodsReceiptPO.Lines.BatchNumbers.AddmisionDate = Now
-                        GoodsReceiptPO.Lines.BatchNumbers.Add()
-
-
-                        GoodsReceiptPO.Lines.Add()
+                        ba.Clear()
+                        quantity.Clear()
+                        oInvGenExit.Lines.BaseType = 202
+                        oInvGenExit.Lines.BaseEntry = docE
+                        oInvGenExit.Lines.BaseLine = contbase
+                        oInvGenExit.Lines.AccountCode = "_SYS00000000039"
+                        WO.Lines.SetCurrentLine(contbase)
+                        frm.load(DGV2.Rows(chk.RowIndex).Cells.Item(1).Value.ToString, wo.Lines.PlannedQuantity, docE)
+                        frm.ShowDialog()
+                        Dim cont As Integer
+                        For cont = 0 To ba.Count - 1
+                            oInvGenExit.Lines.BatchNumbers.BatchNumber = ba.Item(cont)
+                            oInvGenExit.Lines.BatchNumbers.Quantity = quantity.Item(cont)
+                            oInvGenExit.Lines.BatchNumbers.Add()
+                        Next
+                        contbase = contbase + 1
+                        oInvGenExit.Lines.Add()
                     End If
                 Next
-
-            End If
-            '---------------------------------------Ingresa Mercaderia----------------------
-            GoodsReceiptPO.Comments = PO.DocEntry
-            iResult = GoodsReceiptPO.Add()
-            If iResult <> 0 Then
-                MessageBox.Show(con.oCompany.GetLastErrorDescription)
-            Else
-                PO.Comments = PO.DocEntry
-                iResult2 = PO.Update() '---------------------------- Actualiza el pedido (las lineas del pedido)
-                If iResult2 <> 0 Then
+                oReturn = oInvGenExit.Add
+                If oReturn <> 0 Then
                     MessageBox.Show(con.oCompany.GetLastErrorDescription)
                 End If
-                '-------------------------------IMPRIME BATCH--------------------------------------
-                'bat As String, desc As String, anch As Double, pes As Double, bob As String, het As String, coi As String, ordr As String
-                Dim cont As Integer
-                For cont = 0 To batch.Count - 1
-                    'imprime(FormatBarCode(batch.Item(cont)), descripcion(cont), anchotira(cont), pesoreal(cont), bobina(cont), heat(cont), coil(cont), ordencorte(cont))
-                Next
-                '-----------------------------------------------------------------------------------
             End If
-            con.oCompany.Disconnect()
+
         Catch ex As Exception
             MsgBox("Error: " + ex.Message.ToString)
-            con.oCompany.Disconnect()
         End Try
     End Sub
     Private Sub GR_from_PO()
         Try
             If con.MakeConnectionSAP() Then
-                generaEntrada()
+                generaEntrada(txtOrder.Text)
             Else
                 con.MakeConnectionSAP()
                 If con.Connected Then
-                    generaEntrada()
+                    generaEntrada(txtOrder.Text)
                 Else
                     MessageBox.Show("Error de Conexion, intente Nuevamente")
                 End If
@@ -265,8 +195,6 @@ Public Class FrmP
         ElseIf result = DialogResult.Yes Then
             TextBox2.Clear()
             txtOrder.Clear()
-            PO = Nothing
-            GoodsReceiptPO = Nothing
             DGV.DataSource = Nothing
             DGV2.DataSource = Nothing
             MessageBox.Show("Inicie un objeto nuevo")
@@ -283,7 +211,7 @@ Public Class FrmP
                 con.oCompany.Disconnect()
             Catch
             End Try
-            Application.Exit()
+            System.Windows.Forms.Application.Exit()
             Me.Close()
         End If
     End Sub
